1fe18939a9cb1c93ba85a6510c38962f5175fc9f
rica213 <25f5k252l@mozmail.com>
2023-01-30 15:58:07 +0300

    Remove everything from the project

--- src/modules/taskElements.js
+++
@@ -1,4 +0,0 @@
-export const addBtn = document.querySelector('.fa-arrow-right');
-export const clearTasksBtn = document.querySelector('.clear-tasks');
-export const newTask = document.getElementById('new-task');
-export const tasksContainer = document.querySelector('.list-tasks');
